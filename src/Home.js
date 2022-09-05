ee1b2891ebc9269ffafb18e6004c06354b0c64a2
Stefan Miloiu <stefanmiloiu@live.com>
2022-09-05 19:25:02 +0300

    test useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,10 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  useEffect(() => {
+    console.log('use effect TEST');
+  }, []);
 
    return ( 
     <div className="home">
