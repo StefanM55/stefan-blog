c6fa4958f901bebe79008d96c373ba48075643f2
Stefan Miloiu <stefanmiloiu@live.com>
2022-09-07 20:32:42 +0300

    create 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
           </Route>
           <Route path='/blogs/:id'>
             <BlogDetails />
+          </Route>
+          <Route path="*">
+            {/* catch any other routes  */}
+            <NotFound />
           </Route>
         </Switch>    
       </div>
