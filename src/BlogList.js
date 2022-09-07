2d6c68b245cd032c23e2b65749ad5ea0efd9f2c8
Stefan Miloiu <stefanmiloiu@live.com>
2022-09-07 19:32:12 +0300

    add route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   // const blogs = props.blogs; --echivalente cu ce este direct in acolade
   // const title = props.title;
@@ -9,8 +11,10 @@ const BlogList = ({ blogs, title }) => {
       <h2>{ title }</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
